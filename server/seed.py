8356fb6eea27b75c17c4ee6bc1e68a3994fb7c04
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-10-02 01:38:11 +0300

    completed..

--- server/seed.py
+++ server/seed.py
@@ -1,56 +1,18 @@
-#!/usr/bin/env python3
+from app import db, User
 
-from random import randint, choice as rc
+def seed_database():
+    db.create_all()
 
-from faker import Faker
+    # Create a test user with username "Stephorinho $9" and password "password123"
+    user1 = User(username="Stephorinho $9")
+    user1.set_password("password123")
 
-from app import app
-from models import db, Article, User
+    # Create another test user with username "Darwin Nunez" and password "secret456"
+    user2 = User(username="Darwin Nunez")
+    user2.set_password("secret456")
 
-fake = Faker()
-
-with app.app_context():
-
-    print("Deleting all records...")
-    Article.query.delete()
-    User.query.delete()
-
-    fake = Faker()
-
-    print("Creating users...")
-    users = []
-    usernames = []
-    for i in range(25):
-
-        username = fake.first_name()
-        while username in usernames:
-            username = fake.first_name()
-        
-        usernames.append(username)
-
-        user = User(username=username)
-        users.append(user)
-
-    db.session.add_all(users)
-
-    print("Creating articles...")
-    articles = []
-    for i in range(100):
-        content = fake.paragraph(nb_sentences=8)
-        preview = content[:25] + '...'
-        
-        article = Article(
-            author=fake.name(),
-            title=fake.sentence(),
-            content=content,
-            preview=preview,
-            minutes_to_read=randint(1,20),
-            is_member_only = rc([True, False, False])
-        )
-
-        articles.append(article)
-
-    db.session.add_all(articles)
-    
+    db.session.add_all([user1, user2])
     db.session.commit()
-    print("Complete.")
+
+if __name__ == "__main__":
+    seed_database()
